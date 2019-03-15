75b68ffa804982502fe9d8de81fbd35ba59f5b2c
samling <samlingx@gmail.com>
2019-03-15 12:22:51 -0700

    Adding some comments, simplifying some regex by making it case-insensitive

--- main.py
+++ main.py
@@ -7,18 +7,21 @@ import re
 
 client = discord.Client()
 
+# Initial ready state
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
+# Responses for received messages
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    laughter_regexp = re.compile(r'\b(?:(A|a)*(?:(H|h)(A|a))+h?|(?:(L|l)+(O|o)+)+(L|l)+)\b')
+    # Randomly chime in with laughter
+    laughter_regexp = re.compile(r'\b(?:a*(?:ha)+h?|(?:l+o+)+l+)\b', re.IGNORECASE)
     if laughter_regexp.search(message.content):
-        value = randint(0,1000)
+        value = randint(0,100)
         responses = [
             "lol",
             "haha",
@@ -28,29 +31,30 @@ async def on_message(message):
             "hahaha",
             "Hahaha"
         ]
-        print("Detected laughter: {}".format(value))
-        print(value)
-        if (value >= 950):
-            print("Laughing in unison...")
+        if (value <= 5): # 5% chance to respond
             response_idx = randint(0,len(responses) - 1)
             await client.send_message(message.channel, content = responses[response_idx])
     
+    # Apologize if we get annoyed with pugbot
     annoyed_regexp = re.compile(r'\b(god( ?)dam(m?n?i?t?)|fucking)( ?)pugbot\b', re.IGNORECASE)
     if annoyed_regexp.search(message.content):
         await client.send_message(message.channel, content = "Sorry")
 
+    # Express pugbot's inner child
     penis_regexp = re.compile(r'(P|p)enis')
     if penis_regexp.search(message.content):
         await client.send_message(message.channel, content = "Duhuhuhuh")
 
+    # Tell her what time it is
     if "10:33" in message.content:
         await client.send_file(message.channel, 'its_1033/its_1033_final.mp4')
 
+# Tell her what time it is every day at 10:33
 async def tell_her_what_time_it_is():
     await client.wait_until_ready()
     send_time   = "10:33"
     wait_time   = 1
-    channel_id  = "462408914609111082"
+    channel_id  = "462408914609111082" # General; TODO: Find the channel ID programmatically
     channel     = client.get_channel(channel_id)
     mp4_file    = 'its_1033/its_1033_final.mp4'
     while not client.is_closed:
@@ -62,6 +66,7 @@ async def tell_her_what_time_it_is():
             wait_time = 1
         await asyncio.sleep(wait_time)
 
+# Asynchronous task loops
 client.loop.create_task(tell_her_what_time_it_is())
 
 client.run('NTUxNzE4Njc2MzAzMTgzODgy.D2L1sg.Z0DpvoFR4gmB5cs5HkJy8sFEMvw')
